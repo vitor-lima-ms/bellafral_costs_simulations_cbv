9efd1ed722b8115c812add82dbd2e935c66d44e0
Vitor Lima <viitorlima2000@gmail.com>
2025-04-17 03:47:25 -0300

    Trying to solve problems

--- base_dir/settings.py
+++ base_dir/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ['DB_PASSWORD_YO'],
+        'PASSWORD': 'gMlGbIzRkRWOwfBxgruTZdjztwAnJNaI',
         'HOST': 'interchange.proxy.rlwy.net',
         'PORT': '12780',
     }
